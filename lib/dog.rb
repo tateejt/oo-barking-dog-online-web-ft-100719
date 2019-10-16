bc7e0dc7626b602a9c256ea3f786a90bf9290a06
Emery Tate <tateejt@gmail.com>
2019-10-16 23:11:44 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Dog 
+  def bark
+    puts "woof!"
+  end
+  
+  def name=(this_dog_name)
+    @my_name = this_dog_name
+    
+  end
+  
+  def name
+    @my_name
+  end
+end
